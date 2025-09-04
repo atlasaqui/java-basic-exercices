eb967d4c2113f13f6da872e2a35939fb55811715
Atlas <victorwmonteiro@gmail.com>
2025-09-03 22:35:45 -0300

    Add files via upload

---
+++ VerificacaoAcessoSistema.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;    
+ 
+public class VerificacaoAcessoSistema {    
+    public static void main(String[] args) {    
+        Scanner scanner = new Scanner(System.in);    
+        int codigoCorreto = 2023;    
+        int nivelPermissaoMinimo = 1;    
+        int nivelPermissaoMaximo = 3;    
+ 
+        System.out.print("Digite o código de acesso: ");    
+        int codigoDigitado = scanner.nextInt();    
+ 
+        System.out.print("Digite o nível de permissão: ");    
+        int nivelPermissao = scanner.nextInt();    
+ 
+        boolean codigoValido = codigoDigitado == codigoCorreto;  
+        boolean permissaoValida = nivelPermissao >= nivelPermissaoMinimo && nivelPermissao <= nivelPermissaoMaximo;    // Se o nivel de permissão for maior e igual que niver de permissão mínimo e nivel de permisão for menor e igual ao nivel maximo a permissão é válida
+ 
+        if (codigoValido && permissaoValida) {    
+            System.out.println("Acesso permitido. Bem-vindo ao sistema!");    
+        } else {    
+            System.out.println("Acesso negado. Motivo(s):");    
+            if (!codigoValido) {    
+                System.out.println("- Código de acesso incorreto.");    
+            }    
+            if (!permissaoValida) {    
+                System.out.println("- Nível de permissão inválido.");    
+            }    
+        }    
+    }    
+}    
